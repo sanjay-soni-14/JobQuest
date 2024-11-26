1d5d70addbc2eeb2fd6439e48ba3bb704ae58982
sanjay-soni-14 <sanjay@example.com>
2024-11-26 18:44:43 +0530

    Update MongoDB connection configuration

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,6 +5,8 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            retryWrites: true,
+            w: 'majority'
         });
         
         console.log(`MongoDB Connected: ${conn.connection.host}`);
